822dad5085fad566175b526192ff21bf3590f942
Enzo Mendes Golfetti <enzo.golfetti04@gmail.com>
2022-04-25 21:04:53 -0400

    commit archivos basicos

---
+++ business_intelligence_FEN/analisis_fuga_clientes_BI_FEN.py
@@ -0,0 +1,6 @@
+import streamlit as st
+#Agora tem que conseguir puxar o banco lá do Drive com a PyDrive e depois copiar as células que plotam os gráficos
+st.write('# App de soporte para análisis de Fuga de clientes')
+
+
+
