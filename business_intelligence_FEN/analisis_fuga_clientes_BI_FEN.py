b1723452f50ff77877d3309cd8a06eae38988f9e
Enzo Mendes Golfetti <enzo.golfetti04@gmail.com>
2022-04-26 20:21:25 -0400

    commit database

--- business_intelligence_FEN/analisis_fuga_clientes_BI_FEN.py
+++ business_intelligence_FEN/analisis_fuga_clientes_BI_FEN.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+
+
+
 #Agora tem que conseguir puxar o banco lá do Drive com a PyDrive e depois copiar as células que plotam os gráficos
 st.write('# App de soporte para análisis de Fuga de clientes')
 
